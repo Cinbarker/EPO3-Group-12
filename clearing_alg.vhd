51ec0964f1c7bf91a6c623ca96ce4a37d305dd6d
MatthijsSpijker1 <93917786+MatthijsSpijker1@users.noreply.github.com>
2021-12-14 12:32:01 +0100

    Add files via upload
    Added controller, movement and clearing modules

---
+++ clearing_alg.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity clearing_alg is
+	port (	clk 	: in std_logic;
+		reset 	: in std_logic;
+
+		controller_in : in std_logic_vector(2 downto 0);
+	
+		cover_in	: in std_logic;  					--from memory
+		flag_in		: in std_logic;				--from memory
+		mine_in		: in std_logic;				--from seed gen
+		number_in	: in std_logic_vector(2 downto 0);				--from building alg
+
+		cursor_in	: in std_logic_vector(7 downto 0);
+		
+		cursor_out	: out std_logic_vector(7 downto 0);		-- to memory
+
+		cover_out	: out std_logic;					--to memory
+		flag_out	: out std_logic;					--to memory
+
+		game_over	: out std_logic					--to display
+	);
+end entity;
+
